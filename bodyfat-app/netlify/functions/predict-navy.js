87fdfbbff128d3fa3a909f33a990a29793a73f0f
blakiezer <fordreamport@gmail.com>
2025-05-06 11:08:45 +0300

    Update predict-navy.js

--- bodyfat-app/netlify/functions/predict-navy.js
+++ bodyfat-app/netlify/functions/predict-navy.js
@@ -1,13 +1,5 @@
-exports.handler = async (event) => {
-    const { waist, neck, height, gender } = JSON.parse(event.body);
-    let bodyFat;
-    if (gender === 'male') {
-      bodyFat = 86.01 * Math.log10(waist - neck) - 70.041 * Math.log10(height) + 36.76;
-    } else {
-      bodyFat = 163.203 * Math.log10(waist + neck - height) - 97.684 * Math.log10(height) - 78.387;
-    }
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ bodyFat: bodyFat.toFixed(1) })
-    };
-  };
+// Before (Line 5)
+7 86.01 * Math.log10(malst - nec
+
+// After
+bodyFat = 86.01 * Math.log10(waist - neck) - 70.041 * Math.log10(height) + 36.76;
